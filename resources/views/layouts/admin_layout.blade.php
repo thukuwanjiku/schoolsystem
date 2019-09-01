80755da92ae8342c14fdec6c66a981c520e8960c
thukuwanjiku <thukuwanjikum@gmail.com>
2019-09-01 20:17:37 +0300

    Bugs fixes

--- resources/views/layouts/admin_layout.blade.php
+++ resources/views/layouts/admin_layout.blade.php
@@ -38,9 +38,12 @@
     <header class="main-header" style="position:fixed;width:100%">
 
         <!-- Logo -->
-        {{--<a href="index2.html" class="logo">
-            <span class="logo-lg"><b>Admin</b>LTE</span>
-        </a>--}}
+        <a href="index2.html" class="logo">
+            <!-- mini logo for sidebar mini 50x50 pixels -->
+            {{--<span class="logo-mini"><b>A</b>LT</span>--}}
+            <!-- logo for regular state and mobile devices -->
+            <span class="logo-lg">School System</span>
+        </a>
 
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top">
