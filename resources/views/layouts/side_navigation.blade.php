65b21720fa0747eb1c2f7c9fc1ae1d5c4f617e67
thukuwanjiku <thukuwanjikum@gmail.com>
2019-09-01 14:21:53 +0300

    Bugs fixes

--- resources/views/layouts/side_navigation.blade.php
+++ resources/views/layouts/side_navigation.blade.php
@@ -11,7 +11,7 @@
 
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu" data-widget="tree">
-            <li class="header">MAIN NAVIGATION</li>
+            {{--<li class="header">MAIN NAVIGATION</li>--}}
 
             @if(auth()->check() && auth()->user()->role->label === 'admin')
             <li>
